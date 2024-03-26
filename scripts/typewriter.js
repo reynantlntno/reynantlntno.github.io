e79a632410abd88e058b12897c0977acf83fd740
Reynan Tolentino <147963747+reynantlntno@users.noreply.github.com>
2024-03-26 10:01:51 +0800

    update

--- scripts/typewriter.js
+++ scripts/typewriter.js
@@ -1,22 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
+    var introText = document.querySelector('.intro-text');
     var typed = document.getElementById('typewriter');
     var text = typed.innerHTML;
     typed.innerHTML = '';
-  
+
     var i = 0;
     var typing = setInterval(function() {
-      if (i < text.length) {
-        typed.innerHTML += text.charAt(i);
-        i++;
-      } else {
-        clearInterval(typing);
-      }
-    }, 200); 
-  
+        if (i < text.length) {
+            typed.innerHTML += text.charAt(i);
+            i++;
+        } else {
+            clearInterval(typing);
+            // Show intro text after typing ends
+            introText.style.opacity = '1';
+            introText.style.top = '0';
+            // After intro text appears, show other contents
+            showOtherContents();
+        }
+    }, 200);
+
     var cursorVisible = true;
     setInterval(function() {
-      cursorVisible = !cursorVisible;
-      typed.style.borderRightColor = cursorVisible ? 'transparent' : 'grey';
-    }, 500); 
-  });
-  
+        cursorVisible = !cursorVisible;
+        typed.style.borderRightColor = cursorVisible ? 'transparent' : 'grey';
+    }, 500);
+
+    function showOtherContents() {
+        // Select other contents and show them
+        document.querySelectorAll('.content-text, .footer, .shadow-box, .container h1').forEach(function(element) {
+            element.style.opacity = '1';
+            element.style.animation = 'fadeIn 1s ease-in forwards';
+        });
+    }
+});
