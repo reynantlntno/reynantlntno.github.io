5faca016840158e488e2b44c0dad15739b77b7c0
Reynan Tolentino <147963747+reynantlntno@users.noreply.github.com>
2024-05-25 19:54:40 +0800

    Add files via upload

---
+++ zcaf/account.js
@@ -0,0 +1,7 @@
+function showAdminForm() {
+    document.querySelector('.form-container').style.transform = 'translateX(-50%)';
+}
+
+function showUserForm() {
+    document.querySelector('.form-container').style.transform = 'translateX(0)';
+}
