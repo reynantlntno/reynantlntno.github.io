b4b74de3194e08167da2a2f2db9e99b957009dfc
Reynan Tolentino <147963747+reynantlntno@users.noreply.github.com>
2024-06-15 21:21:29 +0800

    Delete zcaf directory

--- zcaf/account.js
+++
@@ -1,7 +0,0 @@
-function showAdminForm() {
-    document.querySelector('.form-container').style.transform = 'translateX(-50%)';
-}
-
-function showUserForm() {
-    document.querySelector('.form-container').style.transform = 'translateX(0)';
-}
