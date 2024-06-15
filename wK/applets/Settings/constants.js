cb8542e19ee2e78e944a399a54cbd9f90be3e954
Reynan Tolentino <147963747+reynantlntno@users.noreply.github.com>
2024-06-15 21:11:44 +0800

    Delete wK directory

--- wK/applets/Settings/constants.js
+++
@@ -1,6 +0,0 @@
-// Constants for default wallpaper URL and SHA-256 hash
-const defaultWallpaper = 'def.png'; // Specify the default wallpaper URL
-const defaultWallpaperHash = 'ca3fc36f02fce1e4cdba87edb01f722a385c81b3a5d8b093326980556f9f0b23'; // Specify the SHA-256 hash of the default wallpaper
-
-// Export the constants for use in other modules
-export { defaultWallpaper, defaultWallpaperHash };
